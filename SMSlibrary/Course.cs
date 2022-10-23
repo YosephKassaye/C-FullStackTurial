5902974cb09105f6c0a26fdd5d0e749cad3d6431
Yoseph kassaye <yadugna@gmail.com>
2022-10-22 18:54:40 -0700

    Course class is added

---
+++ SMSlibrary/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMSlibrary
+{
+    public  class Course
+    {
+        public string Cono { get; set; }
+        public string CoTitle { get; set; }
+        public int CrHr { get; set; }
+    }
+}
